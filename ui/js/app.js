2ac076d1113141a24d760fef4c14242b18d55802
Vibhaj Rajan <vibhaj8@gmail.com>
2022-04-08 13:26:39 +0530

    fix: remove # in url

--- ui/js/app.js
+++ ui/js/app.js
@@ -1,26 +1,28 @@
 var APP = angular.module('APP', ['ngRoute', 'ngSanitize', 'chieffancypants.loadingBar'])
 
-	.config(['$routeProvider', '$httpProvider', function($routeProvider, $httpProvider){
+	.config(['$routeProvider', '$httpProvider', '$locationProvider', function($routeProvider, $httpProvider, $locationProvider){
 
 		$routeProvider
-			.when('/', {templateUrl: 'ui/tpl/view.html', controller: "init"})
+			.when('/', {templateUrl: '/ui/tpl/view.html', controller: "init"})
 
-			.when('/:page', {templateUrl: 'ui/tpl/view.html',
+			.when('/:page', {templateUrl: '/ui/tpl/view.html',
 				controller: ['$scope', '$rootScope', '$routeParams', function($scope, $rootScope, $routeParams) {
 					$rootScope.page = $routeParams.page;
-					$scope.pageURL = 'ui/tpl/'+ $rootScope.page +'.html';
+					$scope.pageURL = '/ui/tpl/'+ $rootScope.page +'.html';
 				}]
 			});
 
 		$httpProvider.defaults.headers.post['Content-Type'] = 'application/json';
 		$httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+		$locationProvider.html5Mode(true);
+
 	}])
 
 	.controller('init', ['$scope', '$rootScope', '$routeParams', function($scope, $rootScope, $routeParams) {
 		$scope.minHeight=$(window).height()-3;
-		$scope.headerURL = 'ui/tpl/header.html';
-		$scope.footerURL = 'ui/tpl/footer.html';
+		$scope.headerURL = '/ui/tpl/header.html';
+		$scope.footerURL = '/ui/tpl/footer.html';
 
 		$scope.menu = [ 'home', 'labs', 'about', 'contact' ];
 		$scope.menu_urls = {
@@ -32,7 +34,7 @@ var APP = angular.module('APP', ['ngRoute', 'ngSanitize', 'chieffancypants.loadi
 		};
 
 		$rootScope.page = 'home';
-		$scope.pageURL = 'ui/tpl/home.html';
+		$scope.pageURL = '/ui/tpl/home.html';
 
 		var d = new Date();
 		d.setDate(d.getDate() - 2);
